ef5e7dc7aaad3b693d7a0be6cb1853fa00857893
Arunav Duggal <arunavduggal07@gmail.com>
2025-08-27 10:36:55 +0530

    solved and tested with sample
    identified positives, negatives and zeroes from integers

---
+++ .github/workflows/question 12.c
@@ -0,0 +1,39 @@
+/*Q12: Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+
+Sample Test Cases:
+Input 1:
+-5
+Output 1:
+Negative
+
+Input 2:
+0
+Output 2:
+Zero
+
+Input 3:
+10
+Output 3:
+Positive
+
+*/
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    if (n>0)
+    {
+        printf("number is positive");
+    }
+    else if (n==0)
+    {
+        printf("number is 0");
+    }
+    else if (n<0)
+    {
+        printf("number is negative");
+    }
+    return 0;
+}
