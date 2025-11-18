352d930a45b701e3d834b80c8147a4098ff6dc46
Arunav Duggal <arunavduggal07@gmail.com>
2025-11-18 12:16:19 +0530

    solved and tested with sample

---
+++ .github/workflows/question 108.c
@@ -0,0 +1,56 @@
+/*Q108: Write a Program to take an integer array nums. Print an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i]. The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+/*
+Sample Test Cases:
+Input 1:
+nums = [1,2,3,4]
+Output 1:
+[24,12,8,6]
+
+Input 2:
+nums = [-1,1,0,-3,3]
+Output 2:
+[0,0,9,0,0]
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, nums[100];
+    long answer[100]; 
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &nums[i]);
+    }
+    long leftProduct = 1;
+    for (int i = 0; i < n; i++)
+    {
+        answer[i] = leftProduct;
+        leftProduct *= nums[i];
+    }
+    long rightProduct = 1;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        answer[i] *= rightProduct;
+        rightProduct *= nums[i];
+    }
+    printf("[");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%ld", answer[i]);
+        if (i < n - 1)
+        {
+            printf(",");
+        }
+    }
+    printf("]\n");
+
+    return 0;
+}
